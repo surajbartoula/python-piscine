66b7da03847428041a4f2640e86664b98a699cf7
suraj bartoula <suraj_bartoula@yahoo.com>
2025-10-25 21:22:00 +0400

    adding ex01

--- python0/ex01/format_ft_time.py
+++ python0/ex01/format_ft_time.py
@@ -6,4 +6,4 @@ timestamp = time.time()
 print("Seconds since January 1, 1970: {:,.4f} or {:.2e} in scientific notation"
 	.format(timestamp, timestamp))
 now = datetime.now()
-print(now.strftime("%b %d %Y"))
+print(now.strftime("%b %d %Y")) # string format time. it's a method of datetime objects that let u format a date or time as a string in any layout you want
