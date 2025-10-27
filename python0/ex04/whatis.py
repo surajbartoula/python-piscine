3d7e4e6cdfffaf9fac519dfcc3660e0bc6b4812e
suraj bartoula <suraj_bartoula@yahoo.com>
2025-10-27 22:42:20 +0400

    adding assertionerror on ex04

--- python0/ex04/whatis.py
+++ python0/ex04/whatis.py
@@ -1,10 +1,17 @@
 import sys
 
-assert len(sys.argv) == 2, "AssertionError: more than one argument is provided"
-arg = int(sys.argv[1])
-if not isinstance(arg, int):
-    raise TypeError("AssertionError: argument is not an integer")
-if (arg % 2 == 0):
-    print("I'm Even.")
-else:
-    print("I'm Odd.")
+try:
+	if len(sys.argv) > 2:
+		raise AssertionError("more than one argument is provided")
+	elif len(sys.argv) == 2:
+		try:
+			arg = int(sys.argv[1])
+		except ValueError:
+			raise AssertionError("argument is not an integer")
+			sys.exit(1)
+		if (arg % 2 == 0):
+			print("I'm Even.")
+		else:
+			print("I'm Odd.")
+except AssertionError as e:
+	print(f"AssertionError: {e}")
