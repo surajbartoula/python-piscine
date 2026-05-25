a0086ca026b3f06f5388343de3a4ffd4acd57b40
suraj bartoula <suraj_bartoula@yahoo.com>
2026-05-25 16:11:30 +0200

    modifying the code with numpy

--- python1/ex00/give_bmi.py
+++ python1/ex00/give_bmi.py
@@ -1,4 +1,5 @@
 """Module to calculate BMI values and check them against a limit threshold."""
+import numpy as np
 
 
 def give_bmi(
@@ -12,17 +13,19 @@ def give_bmi(
         raise TypeError("Both height and weight must be provided as lists.")
     if len(height) != len(weight):
         raise ValueError("Height & weight list must be the same length.")
-    bmi_list = []
-    for h, w in zip(height, weight):
-        if not isinstance(h, (int, float) or not isinstance(w, (int, float))):
-            raise TypeError("All elements must be integers or floats.")
-        if h <= 0:
-            raise ValueError("Height must be greater than zero.")
-        if w <= 0:
-            raise ValueError("Weight cannot be negative")
-        bmi = w / (h**2)
-        bmi_list.append(bmi)
-    return bmi_list
+    try:
+        np_height = np.array(height, dtype=float)
+        np_weight = np.array(weight, dtype=float)
+    except (ValueError, TypeError) as e:
+        raise TypeError(
+            "All elements in height & weight must be numbers."
+        ) from e
+    if np.any(np_height <= 0):
+        raise ValueError("Height cannot be negative.")
+    if np.any(np_weight <= 0):
+        raise ValueError("Weight cannot be negative.")
+    bmi_array = np_weight / (np_height ** 2)
+    return bmi_array.tolist()
 
 
 def apply_limit(bmi: list[int | float], limit: int | float) -> list[bool]:
@@ -34,12 +37,13 @@ def apply_limit(bmi: list[int | float], limit: int | float) -> list[bool]:
         raise TypeError("BMI must be provided as a list.")
     if not isinstance(limit, (int, float)):
         raise TypeError("Limit must be an integer or a float.")
-    limit_results = []
-    for val in bmi:
-        if not isinstance(val, (int, float)):
-            raise TypeError("All elements in BMI list must be numbers.")
-        limit_results.append(val > limit)
-    return limit_results
+    try:
+        np_bmi = np.array(bmi, dtype=float)
+    except (ValueError, TypeError) as e:
+        raise TypeError("Elements in BMI List must be numbers.") from e
+    # Vectorized comparision
+    limit_results = np_bmi > limit
+    return limit_results.tolist()
 
 
 def main():
